db91e31e831ea67330fc13f594c96abf7ef5317c
jdockry1 <32038395+jdockry1@users.noreply.github.com>
2018-05-22 14:35:51 +0100

    User Login Validation Fix

--- server/models/user.js
+++ server/models/user.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema ({
-    username: { type: string, required: true, unique: true },
-    password: { type: string, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     name: { type: string }
 }, { collection: 'user' })
 
